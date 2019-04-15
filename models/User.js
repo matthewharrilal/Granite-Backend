9c220c22aaa3906720ba02015e4f485d75ab2ac0
Matthew Harrilal <matthewharrilal@gmail.com>
2019-04-15 11:46:02 -0700

    Initiating password hashing logic for user's password

--- models/User.js
+++ models/User.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema
-
+const bcrypt = require('bcryptjs')
 
 const UserSchema = new Schema({
     createdAt: {
@@ -32,4 +32,23 @@ const UserSchema = new Schema({
         type: String,
         required: true
     }]
-})
+});
+
+UserSchema.pre("save", function (next) {
+    const now = new Date()
+
+    this.updatedAt = now // If the user schema is being called in anyway update the time its been called
+
+    if (!this.createdAt) {
+        this.createdAt = now // If the schema object is being created for the first time
+    }
+
+    // HASHING OF USER PASSWORD 
+    const user = this;
+    if (!user.isModified('password')) { // If the user hasn't modified password return callback
+        return next() 
+    }
+
+
+
+});
