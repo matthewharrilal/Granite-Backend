433da78594c078fed1963f816725a9071d10d09a
Matthew Harrilal <matthewharrilal@gmail.com>
2019-04-15 12:17:21 -0700

    Most recent push of the server hjs

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
 const server = express();
 const mongoose = require('mongoose');
-const UserModel = require("./models/User");
+const User = require("./models/User");
 const bodyParser = require("body-parser");
